a3c75f64637d62d9e0857b612024a0e72d32c2a7
gregorysysoev <gregorysysoev@gmail.com>
2019-08-05 14:38:03 +1000

    first tests - parser.parse

--- ConsoleUtility.Tests/ParserTests.cs
+++ ConsoleUtility.Tests/ParserTests.cs
@@ -9,15 +9,43 @@ namespace ConsoleUtility.Tests
         [Theory]
         [InlineData("-t")]
         [InlineData("--thread")]
-        [InlineData("--help")]
-        public void Test1(params string[] args)
+        public void Parse_thread_ThreadCommand(params string[] args)
         {
             ConsoleWriter cw = new ConsoleWriter();
 
             Manager manager = new Manager(cw);
             manager.IdentifyCommand(args);
 
-            manager._command.ShouldBeOfType<ICommand>();
+            manager._command.ForEach(x => x.ShouldBeOfType<ThreadSelectCommand>());
+        }
+
+        [Theory]
+        [InlineData("-?")]
+        [InlineData("--help")]
+        public void Parse_Help_HelpCommand(params string[] args)
+        {
+            ConsoleWriter cw = new ConsoleWriter();
+
+            Manager manager = new Manager(cw);
+            manager.IdentifyCommand(args);
+
+            manager._command.ForEach(x => x.ShouldBeOfType<HelpCommand>());
+        }
+
+        [Theory]
+        [InlineData("-?", "-t")]
+        [InlineData("--help", "--thread")]
+        [InlineData("--help", "--thread", "-?", "-t")]
+        public void Parse_HelpAndThread_HelpAndThreadCommands(params string[] args)
+        {
+            ConsoleWriter cw = new ConsoleWriter();
+
+            Manager manager = new Manager(cw);
+            manager.IdentifyCommand(args);
+            foreach (var item in manager._command)
+            {
+                Assert.True(item is ICommand);
+            }
         }
     }
 }
